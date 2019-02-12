a239bc024318087c3596fe67e8ce2064d614ce7c
DESKTOP-NMLP3OO\Sarah Tsai <sarah.y.tsai@gmail.com>
2019-02-12 00:13:40 -0500

    removed gradient and attempted file reading

---
+++ poemReader.js
@@ -0,0 +1,30 @@
+var poems;
+const fs = require('fs')
+var poem = fs.readFile('Poem1.txt', (err, data) => { 
+	if (err) throw err; 
+	var poem = data.toString();
+	var splitPoem = poem.split("/n");
+	for (var i = 0; i < splitPoem.length; i++) {
+        console.log(splitPoem[i]);
+	}
+});
+
+
+// window.addEventListener('load', loadPoems, false);
+// console.log(poems[0]);
+// function loadPoems() {
+// 	var newDiv = window.document.createElement("div");
+// 	newDiv.classList.add("mySlides");
+// 	newDiv.classList.add("fade");
+// 	var textDiv = window.document.createElement("div");
+// 	textDiv.classList.add("text");
+//     for (var i = 0; i < poem[0].length; i++) {
+//     	var newContent = window.document.createTextNode("Hi there and greetings!");
+//     	textDiv.appendChild(newContent);
+//     }
+// 	newDiv.appendChild(textDiv);  
+// 	var currentDiv = window.document.getElementsByClassName("slideshow-container")[0]; 
+// 	currentDiv.appendChild(newDiv);
+// }
+
+
